35fb04d50cf5f16ada7a279e42833c4d0a5c39b7
Oussema Attia <oussema.attia@keyrus.com>
2023-10-20 18:22:36 +0100

    get server

--- routes/users.js
+++ routes/users.js
@@ -12,9 +12,14 @@ router.post('/', (req, res) => {
     ...req.body
   });
   server.save()
-    .then(() => res.status(201).json({ message: 'Success add !'}))
+    .then((server) => res.status(201).json(server))
     .catch(error => res.status(400).json({ error }));
 })
+router.get('/:id', (req, res, next) => {
+  Server.findOne({ _id: req.params.id })
+    .then(server => res.status(200).json(server))
+    .catch(error => res.status(404).json({ error }));
+});
 router.put('/', (req, res) => {
   res.send('Got a PUT request at /user')
 })
