cf6d7efd2f0e8cfee9a97f16cc4178d105ceea00
Oussema Attia <oussema.attia@keyrus.com>
2023-10-20 18:14:01 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,39 +3,16 @@ var router = express.Router();
 const Server = require('../models/Server');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  // res.send('respond with a resource');
-  const stuff = [
-    {
-      _id: 'oeihfzeoi',
-      title: 'Mon premier objet',
-      description: 'Les infos de mon premier objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 4900,
-      userId: 'qsomihvqios',
-    },
-    {
-      _id: 'oeihfzeomoihi',
-      title: 'Mon deuxième objet',
-      description: 'Les infos de mon deuxième objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 2900,
-      userId: 'qsomihvqios',
-    },
-  ];
-  res.status(200).json(stuff);
+  Server.find()
+    .then(servers => res.status(200).json(servers))
+    .catch(error => res.status(400).json({ error }));
 });
 router.post('/', (req, res) => {
-  // res.send('Got a POST request')
-  // console.log(req.body);
-  // res.status(201).json({
-  //   message: 'Objet créé !'
-  // });
-  delete req.body._id;
   const server = new Server({
     ...req.body
   });
   server.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .then(() => res.status(201).json({ message: 'Success add !'}))
     .catch(error => res.status(400).json({ error }));
 })
 router.put('/', (req, res) => {
