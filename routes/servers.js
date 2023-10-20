5f7cf8e9c25ff1e798ef882354d1bff094d783c6
Oussema Attia <oussema.attia@keyrus.com>
2023-10-20 18:34:27 +0100

    Update servers.js

--- routes/servers.js
+++ routes/servers.js
@@ -1,34 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const Server = require('../models/Server');
+const Server = require("../models/Server");
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get("/", function (req, res, next) {
   Server.find()
-    .then(servers => res.status(200).json(servers))
-    .catch(error => res.status(400).json({ error }));
+    .then((servers) => res.status(200).json(servers))
+    .catch((error) => res.status(400).json({ error }));
 });
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   const server = new Server({
-    ...req.body
+    ...req.body,
   });
-  server.save()
+  server
+    .save()
     .then((server) => res.status(201).json(server))
-    .catch(error => res.status(400).json({ error }));
-})
-router.get('/:id', (req, res, next) => {
-  Server.findOne({ _id: req.params.id })
-    .then(server => res.status(200).json(server))
-    .catch(error => res.status(404).json({ error }));
+    .catch((error) => res.status(400).json({ error }));
 });
-router.put('/:id', (req, res) => {
+router.get("/:id", (req, res, next) => {
+  Server.findOne({ _id: req.params.id })
+    .then((server) => res.status(200).json(server))
+    .catch((error) => res.status(404).json({ error }));
+});
+router.put("/:id", (req, res) => {
   Server.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(server => res.status(200).json(server))
-    .catch(error => res.status(400).json({ error }));
-})
-router.delete('/:id', (req, res) => {
+    .then((server) => res.status(200).json(server))
+    .catch((error) => res.status(400).json({ error }));
+});
+router.delete("/:id", (req, res) => {
   Server.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Server deleted !'}))
-    .catch(error => res.status(400).json({ error }));
-})
+    .then(() => res.status(200).json({ message: "Server deleted !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
 
 module.exports = router;
