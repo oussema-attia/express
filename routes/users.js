7b1f9b6fc1bb25b73de668bf21efa88f55013f81
Oussema Attia <oussema.attia@keyrus.com>
2023-10-20 15:36:01 +0100

    first commit

---
+++ routes/users.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+router.post('/', (req, res) => {
+  res.send('Got a POST request')
+})
+router.put('/', (req, res) => {
+  res.send('Got a PUT request at /user')
+})
+router.delete('/', (req, res) => {
+  res.send('Got a DELETE request at /user')
+})
+
+module.exports = router;
