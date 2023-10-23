2c6df2f40fe65867e2633d8b711236c6d4819953
Oussema Attia <oussema.attia@keyrus.com>
2023-10-23 01:59:53 +0100

    update-api

--- routes/servers.js
+++ routes/servers.js
@@ -4,7 +4,17 @@ const Server = require("../models/Server");
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   Server.find()
-    .then((servers) => res.status(200).json(servers))
+    .then((servers) => {
+      const roots = servers.map((server) => {
+        return {
+          id: server._id,
+          title: server.title,
+          description: server.description,
+          isActive: server.isActive,
+        };
+      });
+      res.status(200).json(roots);
+    })
     .catch((error) => res.status(400).json({ error }));
 });
 router.post("/", (req, res) => {
@@ -13,12 +23,22 @@ router.post("/", (req, res) => {
   });
   server
     .save()
-    .then((server) => res.status(201).json(server))
+    .then((server) => res.status(201).json({
+      id: server._id,
+          title: server.title,
+          description: server.description,
+          isActive: server.isActive,
+    }))
     .catch((error) => res.status(400).json({ error }));
 });
 router.get("/:id", (req, res, next) => {
   Server.findOne({ _id: req.params.id })
-    .then((server) => res.status(200).json(server))
+    .then((server) => res.status(200).json({
+      id: server._id,
+          title: server.title,
+          description: server.description,
+          isActive: server.isActive,
+    }))
     .catch((error) => res.status(404).json({ error }));
 });
 router.put("/:id", (req, res) => {
